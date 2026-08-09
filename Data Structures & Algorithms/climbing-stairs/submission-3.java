a01d1308d2a52cb998255535465f667cc79bbaa8
AshaSaini-033 <ashasainiasha0866@gmail.com>
2026-08-09 21:46:43 +0530

    Add: climbing-stairs - submission-3

---
+++ Data Structures & Algorithms/climbing-stairs/submission-3.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public int climbStairs(int n) {
+    
+        int dp[] = new int[n+1];
+        dp[0] = 1;
+        dp[1] = 1;
+       
+        return func(n,dp);
+    }
+    public int func(int n,int dp[]){
+        
+        if(n==0||n==1) return 1;
+        if(n<0) return 0;
+        if(dp[n]!=0) return dp[n];
+        return dp[n]=func(n-1,dp)+func(n-2,dp);
+    }
+}
