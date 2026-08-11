0d3ab3f09349b8000b83164eb0d59747cc7349c9
AshaSaini-033 <ashasainiasha0866@gmail.com>
2026-08-11 14:35:32 +0530

    Add: count-paths - submission-1

---
+++ Data Structures & Algorithms/count-paths/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int dp[][]=new int[m][n];
+   for(int i=0;i<m;i++){
+         Arrays.fill(dp[i],-1);
+   }
+        return func(m-1,n-1,dp);
+    }
+    public int func(int m,int n,int dp[][]){
+        if(m<0 || n<0) return 0;
+       if(m==0 && n==0 ) return 1;
+       if(dp[m][n]!=-1) return dp[m][n];
+       //up
+       int up = func(m-1,n,dp);
+
+       int left = func(m,n-1,dp);
+       return dp[m][n]= up+left;
+    }
+}
