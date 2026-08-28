27bf0c600bf04e764cbaa9f58a9699a59416184d
Asha Saini <ashasainiasha0866@gmail.com>
2026-08-28 14:47:23 +0530

    Add: non-cyclical-number - submission-34

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-34.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isHappy(int n) {
+        int slow = n;
+        int fast = n;
+        do{
+            slow = sumofsq(slow);
+            fast = sumofsq(sumofsq(fast));
+
+        }while(slow!=fast);
+        return slow ==1;
+
+    }
+    public int sumofsq(int n){
+        int sum =0;
+        while(n>0){
+           int r = n%10;
+           sum +=r*r;
+           n = n/10;
+        }
+        return sum;
+    }
+}
